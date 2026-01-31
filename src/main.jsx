d93a051330e4c90899c23f6ef3edc56082818f04
iago.mussel <iago.mussel@ezops.cloud>
2026-01-31 13:46:24 -0300

    Deduplicate global error overlay

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
-import GlobalErrorOverlay from './components/GlobalErrorOverlay.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <GlobalErrorOverlay />
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </>
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 )
